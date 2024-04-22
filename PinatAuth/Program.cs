d032edac682596a6a9c9d572420f4518519750c8
aPinat <aPinat@pinat.dev>
2024-04-22 18:46:41 +0200

    Fetch endpoints from well-known config

--- PinatAuth/Program.cs
+++ PinatAuth/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using PinatAuth;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,13 +15,20 @@ var useRefreshToken = bool.Parse(Environment.GetEnvironmentVariable("USE_REFRESH
 
 var redirectUri = $"{applicationBaseUrl}/login";
 
+var response = await httpClient.GetAsync($"{oAuthBaseUrl}/.well-known/openid-configuration");
+response.EnsureSuccessStatusCode();
+var wellKnown = await response.Content.ReadFromJsonAsync<JsonElement>();
+var tokenEndpoint = wellKnown.GetProperty("token_endpoint").GetString();
+var authorizationEndpoint = wellKnown.GetProperty("authorization_endpoint").GetString();
+var endSessionEndpoint = wellKnown.GetProperty("end_session_endpoint").GetString();
+
 app.MapGet("/login", async context =>
 {
     var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
     if (context.Request.Query.TryGetValue("code", out var code))
     {
         logger.LogInformation("Found authorization_code");
-        var tokenResponse = await httpClient.PostAsync($"{oAuthBaseUrl}/oauth2/token",
+        var tokenResponse = await httpClient.PostAsync(tokenEndpoint,
             new FormUrlEncodedContent(new Dictionary<string, string>
             {
                 { "client_id", clientId },
@@ -62,7 +70,7 @@ app.MapGet("/login", async context =>
     else if (useRefreshToken && context.Request.Cookies.TryGetValue("refresh_token", out var refreshToken))
     {
         logger.LogInformation("Found refresh_token");
-        var tokenResponse = await httpClient.PostAsync($"{oAuthBaseUrl}/oauth2/token",
+        var tokenResponse = await httpClient.PostAsync(tokenEndpoint,
             new FormUrlEncodedContent(new Dictionary<string, string>
             {
                 { "client_id", clientId }, { "client_secret", clientSecret }, { "grant_type", "refresh_token" }, { "refresh_token", refreshToken! }
@@ -95,12 +103,12 @@ app.MapGet("/login", async context =>
         if (useRefreshToken)
         {
             logger.LogInformation("No code or refresh_token found");
-            context.Response.Redirect($"{oAuthBaseUrl}/oauth2/authorize?client_id={clientId}&response_type=code&scope=offline_access&redirect_uri={redirectUri}");
+            context.Response.Redirect($"{authorizationEndpoint}?client_id={clientId}&response_type=code&scope=offline_access&redirect_uri={redirectUri}");
         }
         else
         {
             logger.LogInformation("No code found");
-            context.Response.Redirect($"{oAuthBaseUrl}/oauth2/authorize?client_id={clientId}&response_type=code&redirect_uri={redirectUri}");
+            context.Response.Redirect($"{authorizationEndpoint}?client_id={clientId}&response_type=code&redirect_uri={redirectUri}");
         }
     }
 });
@@ -109,7 +117,7 @@ app.MapGet("/logout", context =>
 {
     context.Response.Cookies.Delete("access_token", new CookieOptions { Domain = domain, HttpOnly = true });
     context.Response.Cookies.Delete("refresh_token", new CookieOptions { Domain = domain, HttpOnly = true });
-    context.Response.Redirect($"{oAuthBaseUrl}/oauth2/logout?client_id={clientId}&post_logout_redirect_uri={applicationBaseUrl}/login");
+    context.Response.Redirect($"{endSessionEndpoint}?client_id={clientId}&post_logout_redirect_uri={applicationBaseUrl}/login");
     return Task.CompletedTask;
 });
 
